60d82e5ea2a872bb71868e95b3891b284862ea31
Marc Alexander <admin@m-a-styles.de>
2011-01-05 21:35:53 +0100

    Added some more code

--- phpBB_jQuery_Base/root/jquery_base.php
+++ phpBB_jQuery_Base/root/jquery_base.php
@@ -15,17 +15,23 @@ define('IN_JQUERY_BASE', true);
 $phpbb_root_path = './';
 $phpEx = substr(strrchr(__FILE__, '.'), 1);
 include($phpbb_root_path . 'common.'.$phpEx);
-//include($phpbb_root_path . 'includes/functions_display.' . $phpEx);
-//include($phpbb_root_path . 'includes/message_parser.' . $phpEx);
+include($phpbb_root_path . 'jquery_base/jquery_functions.' . $phpEx);
 
 // Start session management
 $user->session_begin();
 $auth->acl($user->data);
 $user->setup('posting');
 
+// create new instance of jQuery Base class
+$jquery_base = new phpbb_jquery_base();
 
+// init jQuery Base
+$jquery_base->init();
 
+// run PHP code for jQuery
+$jquery_base->run_actions();
 
-
+// finish it!!!
+$jquery_base->page_footer();
 
 ?>
--- phpBB_jQuery_Base/root/jquery_base/jquery_functions.php
+++ phpBB_jQuery_Base/root/jquery_base/jquery_functions.php
@@ -16,13 +16,95 @@ if(!defined('IN_PHPBB') || !defined('IN_JQUERY_BASE'))
 }
 
 
-class jquery_base
+class phpbb_jquery_base
 {
+	/*
+	* initial vars
+	*/
+	var $error = array(); // save errors in here (i.e. when quickediting posts)
+	var $mode;
+	var $post_id;
+	var $location;
+	var $submit = false;
+	
+	/* 
+	* function definitions below
+	*/
+	
+	/*
+	* initialise variables for following actions
+	*/
+	function init()
+	{
+		global $user, $phpbb_root_path, $phpEx;
+		
+		$this->post_id = request_var('post_id', 0);
+		$this->mode = request_var('mode', '');
+		$this->location = request_var('location', '');
+		$this->submit = request_var('submit', false);
+		
+		// include needed files
+		switch($this->mode)
+		{
+			case 'quickreply':
+			case 'quickedit':
+				include($phpbb_root_path . 'includes/functions_display.' . $phpEx);
+				include($phpbb_root_path . 'includes/message_parser.' . $phpEx);
+			break;
+			case 'markread_forum':
+				// check what files we need
+			break;
+			default:
+				$error[] = 'NO_MODE';
+		}
+	}
+	
+	/*
+	* Run actions for the specified mode
+	*
+	* @param: none
+	*/
+	function run_actions()
+	{
+		switch($this->mode)
+		{
+			case 'quickreply':
+				$this->quickreply;
+			break;
+			case 'quickedit':
+				$this->quickedit;
+			break;
+			case 'markread_forum':
+				$this->mark_read('forum');
+			break;
+		}
+	}
+	
+	/*
+	*
+	*/
+	function quickreply()
+	{
+	
+	}
 
+	/*
+	*
+	*/
+	function quickedit()
+	{
+	
+	}
 
-
-
-
+	/*
+	* mark forums read
+	*
+	* @param <string> $type The kind of "mark" this function should run (i.e. mark all forums read)
+	*/
+	function mark_read($type)
+	{
+	
+	}
 
 
 }
